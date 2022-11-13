103d5ecd48fa7e89921beac660ca513fe8d3c8d7
Mila <milaivanova2609@gmail.com>
2022-11-13 19:56:17 +0200

    HW completed

---
+++ Classes/FruitsAndVegetables.cs
@@ -0,0 +1,21 @@
+﻿
+namespace ADO.NET
+{
+    public class FruitsAndVegetables
+    {
+        public int ID { get; set; }
+        public string ? Name { get; set; }
+        public string ? Type { get; set; }
+        public string ? Color { get; set; }
+        public int ? CaloricContent { get; set; }
+
+        public FruitsAndVegetables(int iD, string? name, string? type, string? color, int? caloricContent)
+        {
+            ID = iD;
+            Name = name;
+            Type = type;
+            Color = color;
+            CaloricContent = caloricContent;
+        }
+    }
+}
--- Pages/HW.xaml.cs
+++ Pages/HW.xaml.cs
@@ -1,17 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace ADO.NET
 {
@@ -20,9 +11,43 @@ namespace ADO.NET
     /// </summary>
     public partial class HW : UserControl
     {
+        private List<FruitsAndVegetables> fruitsAndVegetables;
         public HW()
         {
             InitializeComponent();
+            fruitsAndVegetables = new List<FruitsAndVegetables>();
+        }
+        private void Back_Click(object sender, RoutedEventArgs e) => Switcher.Switch(new Main());
+        private void HW_Loaded(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string connectionString = @"Data Source=WIN-DB5IFPI2TC8\SQLEXPRESS;Initial Catalog=FruitsAndVegetables;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+                SqlConnection connection = new SqlConnection(connectionString);
+                connection.Open();
+                string command = @"SELECT * FROM Info";
+                SqlCommand sqlCommand = new SqlCommand(command, connection);
+                SqlDataReader reader = sqlCommand.ExecuteReader();
+                while (reader.Read())
+                {
+                    fruitsAndVegetables.Add(new FruitsAndVegetables(reader.GetInt32(0), reader.GetString(1), reader.GetString(2), reader.GetString(3), reader.GetInt32(4)));
+                }
+                HWDG.ItemsSource = fruitsAndVegetables;
+                connection.Close();
+            }
+            catch (SqlException ex)
+            {
+                StringBuilder errorMessages = new StringBuilder();
+                for (int i = 0; i < ex.Errors.Count; i++)
+                {
+                    errorMessages.Append("Index #" + i + "\n" +
+                        "Message: " + ex.Errors[i].Message + "\n" +
+                        "LineNumber: " + ex.Errors[i].LineNumber + "\n" +
+                        "Source: " + ex.Errors[i].Source + "\n" +
+                        "Procedure: " + ex.Errors[i].Procedure + "\n");
+                }
+                MessageBox.Show(errorMessages.ToString());
+            }
         }
     }
 }
