c4a1c8a350a647a7d23589752bdc318d2e872428
Mila <milaivanova2609@gmail.com>
2022-11-21 20:20:29 +0200

    Lab and HW completed

---
+++ Goods.cs
@@ -0,0 +1,22 @@
+﻿namespace ADO.NET
+{
+    public class Goods
+    {
+        public int ID { get; set; }
+        public string ? Name { get; set; }
+        public string ? Type { get; set; }
+        public string ? Supplier { get; set; }
+        public string ? Quantity { get; set; }
+        public string ? Date { get; set; }
+
+        public Goods(int iD, string? name, string? type, string? supplier, string? quantity, string? date)
+        {
+            ID = iD;
+            Name = name;
+            Type = type;
+            Supplier = supplier;
+            Quantity = quantity;
+            Date = date;
+        }
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,28 +1,48 @@
-﻿using System;
+﻿using Microsoft.Data.SqlClient;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace ADO.NET
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private List<Goods> Goods;
         public MainWindow()
         {
             InitializeComponent();
+            Goods = new List<Goods>();
+        }
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string connectionString = @"Data Source=WIN-DB5IFPI2TC8\SQLEXPRESS;Initial Catalog=Warehouse;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+                SqlConnection connection = new SqlConnection(connectionString);
+                connection.Open();
+                string command = @"SELECT * FROM Goods";
+                SqlCommand sqlCommand = new SqlCommand(command, connection);
+                SqlDataReader reader = sqlCommand.ExecuteReader();
+                while (reader.Read())
+                {
+                    Goods.Add(new Goods(reader.GetInt32(0), reader.GetString(1), reader.GetString(2), reader.GetString(3), reader.GetString(4), reader.GetString(5)));
+                }
+                DG.ItemsSource = Goods;
+                connection.Close();
+            }
+            catch (SqlException ex)
+            {
+                StringBuilder errorMessages = new StringBuilder();
+                for (int i = 0; i < ex.Errors.Count; i++)
+                {
+                    errorMessages.Append("Index #" + i + "\n" +
+                        "Message: " + ex.Errors[i].Message + "\n" +
+                        "LineNumber: " + ex.Errors[i].LineNumber + "\n" +
+                        "Source: " + ex.Errors[i].Source + "\n" +
+                        "Procedure: " + ex.Errors[i].Procedure + "\n");
+                }
+                MessageBox.Show(errorMessages.ToString());
+            }
         }
     }
 }
