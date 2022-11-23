d3b3c48788f2836f5925765ea6dd6230eac4942d
Mila <milaivanova2609@gmail.com>
2022-11-23 14:14:30 +0200

    Front part completed

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -15,14 +15,21 @@ using System.Windows.Shapes;
 
 namespace ADO.NET
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ShowAll_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
