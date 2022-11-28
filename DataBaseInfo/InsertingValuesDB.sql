14360cca5771c080de2cf54b72ebceb374f51d28
Mila <101627933+MilaGttP@users.noreply.github.com>
2022-11-28 02:57:43 +0200

    Add files via upload

---
+++ DataBaseInfo/InsertingValuesDB.sql
@@ -0,0 +1,7 @@
+
+INSERT INTO Assessment VALUES
+('Stepanenko Roman Viktorovych', 'VB123', 10, 12, 8, 'Math', 'Geography'),
+('Pavlenko Vikroriya Yuriivna', 'VB123', 9, 9, 8, 'English', 'Math'),
+('Derkach Valentina Ivanivna', 'UT452', 11, 11, 10, 'DB Theory', 'Programming'),
+('Solovey Ivan Grigorovich', 'VB123', 8, 9, 7, 'Math', 'Management'),
+('Zubenko Myhail Petrovich', 'UP322', 10, 10, 9, 'English', 'Management')
